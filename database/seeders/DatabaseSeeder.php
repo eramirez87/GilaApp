d52a9ea5a6f595e77fc02a8723910a9480a53855
Edgar Edel Ramirez <47254183+eramirez87@users.noreply.github.com>
2024-03-21 15:01:00 -0600

    Ajustes al seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Channel;
+
+use App\Models\category_user;
+use App\Models\channel_user;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +20,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        #Creacion de usuarios
         User::factory()->create([
             'name' => 'John Doe',
             'email' => 'demo@demo.com',
@@ -25,6 +29,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Jeane Doe',
             'email' => 'demo2@demo.com',
         ]);
+
+        #Creacion de categorias
         Category::create([
             'name'=>'Deportes'
         ]);
@@ -34,6 +40,8 @@ class DatabaseSeeder extends Seeder
         Category::create([
             'name'=>'Películas'
         ]);
+
+        #Creacion de canales
         Channel::create([
             'name'=>'SMS'
         ]);
@@ -42,6 +50,34 @@ class DatabaseSeeder extends Seeder
         ]);
         Channel::create([
             'name'=>'Push Notification'
+        ]);
+
+        #Creacion de relacion Usuario-Categoria
+        category_user::create([
+            'user_id' => 1,
+            'category_id' => 1
+        ]);
+        category_user::create([
+            'user_id' => 2,
+            'category_id' => 1
+        ]);
+        category_user::create([
+            'user_id' => 1,
+            'category_id' => 3
+        ]);
+
+        #Creacion de relacion Usuario-Canal
+        channel_user::create([
+            'user_id' => 1,
+            'channel_id'=> 2
+        ]);
+        channel_user::create([
+            'user_id' => 1,
+            'channel_id'=> 3
+        ]);
+        channel_user::create([
+            'user_id' => 2,
+            'channel_id'=> 1
         ]);
 
     }
